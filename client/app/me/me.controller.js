6eb64d985d34dc8351511f83554792e5d4ab2b4c
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-08-12 00:45:07 +0900

    Add アップロードボタン

--- client/app/me/me.controller.js
+++ client/app/me/me.controller.js
@@ -1,18 +1,29 @@
 'use strict';
 
 angular.module('sampleApp')
-  .controller('MeCtrl', function ($scope, $http) {
+  .controller('MeCtrl', function ($scope, $http, Upload) {
     $scope.photos = [];
 
-    $http.get('/api/photos/me').success(function(photos) {
+    $http.get('/api/photos/me').success(function (photos) {
       $scope.photos = photos;
     });
 
-    $scope.deletePhoto = function(photo) {
-      $http.delete('/api/photos/me/' + photo._id).success(function(photos) {
+    $scope.deletePhoto = function (photo) {
+      $http.delete('/api/photos/me/' + photo._id).success(function (photos) {
         var index = $scope.photos.indexOf(photo);
         $scope.photos.splice(index, 1);
       });
     }
+
+    $scope.upload = function (file) {
+      Upload.upload({
+        url: '/api/photos/me',
+        file: file
+      }).success(function (data, status, headers, config) {
+        console.log('success :)');
+      }).error(function (data, status, headers, config) {
+        console.log('error status: ' + status);
+      });
+    };
 
   });
