07486776f2f9192926bbb77d114d6959242859fc
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-08-12 00:43:37 +0900

    Add ng-file-upload

--- client/app/app.js
+++ client/app/app.js
@@ -5,7 +5,8 @@ angular.module('sampleApp', [
   'ngResource',
   'ngSanitize',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ngFileUpload'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
