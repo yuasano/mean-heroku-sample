53603d1dbaadf64c888c14d66c5131609f2c8d5e
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-08-11 23:33:54 +0900

    Add プライベート写真表示＆削除

--- client/app/me/me.controller.js
+++ client/app/me/me.controller.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('sampleApp')
-  .controller('MeCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('MeCtrl', function ($scope, $http) {
+    $scope.photos = [];
+
+    $http.get('/api/photos/me').success(function(photos) {
+      $scope.photos = photos;
+    });
+
+    $scope.deletePhoto = function(photo) {
+      $http.delete('/api/photos/me/' + photo._id).success(function(photos) {
+        var index = $scope.photos.indexOf(photo);
+        $scope.photos.splice(index, 1);
+      });
+    }
+
   });
